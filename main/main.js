a3f26cd3e51617273b628ade910df3ea25fe454f
lxxhack <1572411660@qq.com>
2017-06-14 14:24:03 +0800

    Create main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,34 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(input) {
+    var num0=[0,1,0,2,0,2,2,1,2];
+    var num1=[0,0,0,0,0,2,0,0,2];
+    var num2=[0,1,0,0,1,2,2,1,0];
+    var num3=[0,1,0,0,1,2,0,1,2];
+    var num4=[0,0,0,2,1,2,0,0,2];
+    var num5=[0,1,0,2,1,0,0,1,2];
+    var num6=[0,1,0,2,1,0,2,1,2];
+    var num7=[0,1,0,0,0,2,0,0,2];
+    var num8=[0,1,0,2,1,2,2,1,2];
+    var num9=[0,1,0,2,1,2,0,0,2];
+    var num=[num0,num1,num2,num3,num4,num5,num6,num7,num8,num9];
+    num.push.apply(num,num0);
+    var digit=input.toString();
+    for (var k=0;k<3;k++)
+    {
+        for (var i=0;i<digit.length;i++)
+        {
+           for (var j=0;j<3;j++)
+            {
+                switch(num[parseInt(digit.charAt(i))][k*3+j])
+                {
+                    case 0:console.log('.');
+                        break;
+                    case 1:console.log('_');
+                        break;
+                    case 2:consoe.log('|');
+                        break;
+                }
+            }
+        }
+        console.log('\n');
+   }
 };
